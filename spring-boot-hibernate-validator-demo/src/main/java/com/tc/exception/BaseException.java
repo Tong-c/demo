77bf94688a9e66190d5d1d3062a1f0fee2c1cdcc
tongchuang <tongchuang@qdama.cn>
2021-06-08 20:51:52 +0800

    添加 BaseException

---
+++ spring-boot-hibernate-validator-demo/src/main/java/com/tc/exception/BaseException.java
@@ -0,0 +1,130 @@
+package com.zhuizhi.base.exception;
+
+import com.zhuizhi.base.constants.utils.HttpStatus;
+
+public abstract class BaseException extends RuntimeException {
+
+    private int internalErrorCode;
+    private String internalErrorMessage;
+    private int errorCode;
+    private String errorMessage;
+    private Object data;
+    private int httpCode;
+    private String customInfo;
+
+    public BaseException() {
+
+    }
+
+    public BaseException(
+            int errorCode,
+            String errorMessage,
+            Throwable cause,
+            Object data,
+            int httpCode,
+            String customInfo) {
+        super(errorMessage, cause);
+
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.httpCode = httpCode;
+        this.data = data;
+        this.internalErrorCode = errorCode;
+        this.internalErrorMessage = errorMessage;
+        this.customInfo = customInfo;
+    }
+
+    public BaseException(
+            int errorCode,
+            String errorMessage,
+            Throwable cause,
+            Object data,
+            String customInfo) {
+        super(errorMessage, cause);
+
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.httpCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
+        this.data = data;
+        this.internalErrorCode = errorCode;
+        this.internalErrorMessage = errorMessage;
+        this.customInfo = customInfo;
+    }
+
+    public BaseException(
+            int errorCode,
+            String errorMessage,
+            Throwable cause,
+            Object data,
+            int httpCode,
+            int internalErrorCode,
+            String internalErrorMessage,
+            String customInfo) {
+        super(errorMessage, cause);
+
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.httpCode = httpCode;
+        this.data = data;
+        this.internalErrorCode = internalErrorCode;
+        this.internalErrorMessage = internalErrorMessage;
+        this.customInfo = customInfo;
+    }
+
+    public String getCustomInfo() {
+        return customInfo;
+    }
+
+    public void setCustomInfo(String customInfo) {
+        this.customInfo = customInfo;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public int getHttpCode() {
+        return httpCode;
+    }
+
+    public void setHttpCode(int httpCode) {
+        this.httpCode = httpCode;
+    }
+
+    public int getInternalErrorCode() {
+        return internalErrorCode;
+    }
+
+    public void setInternalErrorCode(int internalErrorCode) {
+        this.internalErrorCode = internalErrorCode;
+    }
+
+    public String getInternalErrorMessage() {
+        return internalErrorMessage;
+    }
+
+    public void setInternalErrorMessage(String internalErrorMessage) {
+        this.internalErrorMessage = internalErrorMessage;
+    }
+
+}
---
+++ spring-boot-hibernate-validator-demo/src/main/java/com/tc/exception/BaseKnownException.java
@@ -0,0 +1,102 @@
+package com.zhuizhi.base.exception;
+
+import com.zhuizhi.base.constants.ErrorConstant;
+
+import java.text.MessageFormat;
+
+public class BaseKnownException extends BaseException {
+
+    public BaseKnownException() {
+
+    }
+
+    public BaseKnownException(
+            int errorCode,
+            String errorMessage,
+            Throwable cause,
+            Object data,
+            int httpCode,
+            String customInfo) {
+        super(errorCode, errorMessage, cause, data, httpCode, customInfo);
+    }
+
+    public BaseKnownException(
+            int errorCode,
+            String errorMessage,
+            Throwable cause,
+            Object data,
+            String customInfo) {
+        super(errorCode, errorMessage, cause, data, customInfo);
+    }
+
+    public BaseKnownException(
+            int errorCode,
+            String errorMessage,
+            Throwable cause,
+            Object data,
+            int httpCode,
+            int internalErrorCode,
+            String internalErrorMessage,
+            String customInfo) {
+        super(errorCode, errorMessage, cause, data, httpCode, internalErrorCode, internalErrorMessage, customInfo);
+    }
+
+    public BaseKnownException(int errorCode, String errorMessage) {
+
+        this(errorCode,
+                errorMessage,
+                null,
+                null,
+                ErrorConstant.HTTPCODE_INTERNAL_SERVER_ERROR,
+                errorCode,
+                errorMessage,
+                null);
+
+    }
+
+    /**
+     * 提供可以携带customInfo的异常
+     *
+     * @param errorCode
+     * @param errorMessage
+     * @param customInfo   这个会在最后的log里打印出来，便于排查问题
+     */
+    public BaseKnownException(int errorCode, String errorMessage, String customInfo) {
+
+        this(errorCode,
+                errorMessage,
+                null,
+                null,
+                ErrorConstant.HTTPCODE_INTERNAL_SERVER_ERROR,
+                errorCode,
+                errorMessage,
+                customInfo);
+
+    }
+
+    /**
+     * 返回格式化字符串的Exception
+     *
+     * @param errorCode
+     * @param errorMessage
+     * @return
+     */
+    public static BaseKnownException getFormattedException(int errorCode, String errorMessage, Object... arguments) {
+        return new BaseKnownException(errorCode,
+                MessageFormat.format(errorMessage, arguments));
+    }
+
+    public static BaseKnownException getUserErrorException(int errorCode, String errorMessage) {
+
+        return new BaseKnownException(errorCode,
+                errorMessage,
+                null,
+                null,
+                ErrorConstant.HTTPCODE_INTERNAL_SERVER_ERROR,
+                errorCode,
+                errorMessage,
+                null);
+
+    }
+
+}
