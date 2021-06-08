bd4171d280164ca6f4db897d7ae75cb43adb6062
tong <1972376180@qq.com>
2021-06-08 10:24:15 +0800

    data-permission-demo

---
+++ data-permission-demo/src/main/java/com/tc/DataPermissionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPermissionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataPermissionDemoApplication.class, args);
+    }
+
+}
